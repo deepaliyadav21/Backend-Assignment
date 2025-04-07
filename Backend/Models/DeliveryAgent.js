89585a8edbe73f65ba63867fcf289b1b04011cdb
Deepali Yadav <119069009+deepaliyadav21@users.noreply.github.com>
2025-04-07 13:07:07 +0530

    Delete Backend directory

--- Backend/Models/DeliveryAgent.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const agentSchema = new mongoose.Schema({
-    name:String,
-    ownerId:String,
-    available:Boolean,
-})
-
-module.exports = mongoose.model("agent",agentSchema);
